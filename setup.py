799c40b2ad4a8a7188c651327d436bf9d0cfbc3f
gerrie_vermeer <sander_vrijenhoek@hotmail.com>
2020-12-01 17:17:23 +0100

    Updated package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Microtransat Software Framework',
-      version='0.1',
+      version='0.1.1',
       description='Microtransat Software Framework',
       author='Hogeschool Rotterdam',
       url='https://www.hogeschoolrotterdam.nl/',
