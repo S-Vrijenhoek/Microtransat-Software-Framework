71a76a17896de0426c173821340a26b3562dac0d
gerrie_vermeer <sander_vrijenhoek@hotmail.com>
2020-12-01 16:59:26 +0100

    Updated setup.py packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
-setup(name='Distutils',
+setup(name='Microtransat Software Framework',
       version='0.1',
       description='Microtransat Software Framework',
       author='Hogeschool Rotterdam',
       url='https://www.hogeschoolrotterdam.nl/',
-      packages=['poseidon'],
+      packages=['Microtransat-Software-Framework.poseidon'],
       install_requires=['flake8', 'ruamel.yaml'],)
