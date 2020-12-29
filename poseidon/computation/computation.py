66e148a055d74df5004655d78c2acdb0138d5e08
gerrie_vermeer <sander_vrijenhoek@hotmail.com>
2020-12-29 18:33:18 +0100

    Added helper class

--- poseidon/computation/computation.py
+++ poseidon/computation/computation.py
@@ -1,16 +1,5 @@
 from poseidon.computation.computation_base import ComputationBase
-
-
-def is_between_angles(n, alpha, beta) -> float:
-    if alpha < beta:
-        return alpha <= n <= beta
-    return alpha <= n or n <= beta
-
-
-def distance_between_angles(alpha, beta) -> float:
-    phi = abs(beta - alpha) % 360
-    distance = (180 - phi) % 360 if phi > 90 else phi
-    return distance
+from poseidon.helper import Helper
 
 
 class Computation(ComputationBase):
@@ -18,15 +7,15 @@ class Computation(ComputationBase):
     def compute_optimal_saling_angle(self,
                                      sailboat_rotation: float,
                                      wind_direction: float) -> float:
-        distance = distance_between_angles((sailboat_rotation + 180) % 360, wind_direction)
+        distance = Helper.distance_between_angles((sailboat_rotation + 180) % 360, wind_direction)
 
         if distance > 180:
             distance = (360 - distance) % 360
 
         # If wind blows from starboard
-        if is_between_angles((sailboat_rotation - 180) % 360,
-                             sailboat_rotation,
-                             wind_direction):
+        if Helper.is_between_angles((sailboat_rotation - 180) % 360,
+                                    sailboat_rotation,
+                                    wind_direction):
             distance = -distance
 
         return distance / 2
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='Microtransat Software Framework',
-      version='0.2.1',
+      version='0.2.2',
       description='Microtransat Software Framework',
       author='Hogeschool Rotterdam',
       url='https://www.hogeschoolrotterdam.nl/',
